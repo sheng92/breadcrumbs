4c41f5bb387616390aae1a5c616af6c31a09aad1
Sheng Jiang <sheng92@gmail.com>
2018-03-10 13:37:46 -1000

    Trying smoothing again

--- docs/js/geo.js
+++ docs/js/geo.js
@@ -1,5 +1,5 @@
-currentHeading = {"degrees": 0};
-var zoom = .005;
+var currentHeading = {"degrees": 0};
+var zoom = 10000;
 var watchID;
 var geoLoc;
 var currentTrip = [];
@@ -14,8 +14,7 @@ gn.init().then(function(){
   });
   }).catch(function(e){
   // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-  alert(e);
-  currentHeading.degrees += degrees;
+  console.log(e);
 });
 
 
@@ -49,7 +48,7 @@ function draw() {
     ctx.translate(canvas.width/2, canvas.height/2);
     ctx.rotate(currentHeading.degrees/180*Math.PI);
     ctx.beginPath();
-    for (var i=0, coord; coord = currentTrip[i]; i++) {
+    for (var i=0, coord; coord = currentTripSmooth[i]; i++) {
       var xy = getXY(canvas.height, canvas.width, currentPos.lat, currentPos.long, coord.lat, coord.long, zoom);
       if (i==0){
         ctx.moveTo(xy.x, xy.y);
@@ -69,9 +68,10 @@ function draw() {
 }
 
 function showLocation(position) {
-  var latitude = position.coords.latitude;
-  var longitude = position.coords.longitude;
+  var latitude = position.coords.latitude * 1000000;
+  var longitude = position.coords.longitude * 1000000;
   currentTrip.push({ lat: latitude, long : longitude });
+  currentTripSmooth = simplify(currentTrip, .8, false);
   currentPos = {lat: latitude, long: longitude};
   dLog.innerHTML = '{"lat":' + latitude + ',"long":' + longitude + "}";
   draw();
